8d4b28a4ac16d10cea9131126d77a89d8f0202af
Nguyễn Đăng Tiến <131792899+TienNguyen0712@users.noreply.github.com>
2026-02-22 20:42:00 +0700

    Update preprocess.py

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -7,13 +7,13 @@ def load_data(path):
     return pd.read_csv(f"data/{path}.csv")
 
 def split_scale_data(df):
-    features = df.drop('target', axis=1) # Thay 'target' bằng tên cột mục tiêu của bạn
+    features = df.drop(columns=["Class", "Time", "Amount"])
     target = df['Class']
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, statify=y)
-    
     scaler = RobustScaler()
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
+    features['Amount_log'] = scaler.fit_transform(features['Amount_log'].values.reshape(-1, 1))
     
-    return X_train_scaled, X_test_scaled, y_train, y_test, scaler
+    X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42, stratify=target)
+    
+    return X_train, X_test, y_train, y_test, scaler
+
